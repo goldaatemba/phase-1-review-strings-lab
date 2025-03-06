f506811c5f87468fa851da8597da21abbf615269
golda <goldaatemba610@gmail.com>
2025-03-06 07:49:12 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+console.log(welcomeMessage);
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+console.log(excitedWelcomeMessage);
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+console.log(shortGreeting);
